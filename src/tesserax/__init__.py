b3b6b3914c9ca38bebc775b1ec5f572246898b8a
Alejandro Piad <apiad@apiad.net>
2026-01-31 13:33:04 -0500

    Bump version to 0.3.0

--- src/tesserax/__init__.py
+++ src/tesserax/__init__.py
@@ -2,4 +2,4 @@ from .canvas import Canvas
 from .core import Shape, Bounds, Point
 from .base import Rect, Square, Circle, Ellipse, Line, Arrow, Group, Path
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
